8debfb25c37e1c490a1135cb6f1c6826d96b8e5c
Vitaliy Kucheryaviy <ppr.vitaly@gmail.com>
2024-06-16 15:08:13 +0000

    initial

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -l
+
+GH_TOKEN=$1
+JOB_NAME=$2
+
+# param 3 is minutes, we wait 10seconds between attemnts:
+MAX_ATTEMPTS=$(($3 * 6))
+ATTEMPTS=0
+
+echo "Installing GitHub CLI..."
+curl -sS https://webi.sh/gh | sh
+PATH=$PATH:~/.local/bin/
+
+echo "Waiting for $JOB_NAME to complete..."
+W_JOB_CONCLUSION=$(gh api -X GET /repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs -q '.jobs[] | select(.name=="'"$JOB_NAME"'") | .conclusion')
+until [ -n "$W_JOB_CONCLUSION" ] || [ $ATTEMPTS -ge $MAX_ATTEMPTS ]; do
+  echo "$JOB_NAME is $W_JOB_CONCLUSION ... Attempt $((ATTEMPTS+1))/$MAX_ATTEMPTS"
+  sleep 10
+  ATTEMPTS=$((ATTEMPTS+1))
+  W_JOB_CONCLUSION=$(gh api -X GET /repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs -q '.jobs[] | select(.name=="'"$JOB_NAME"'") | .conclusion')
+done
+
+if [ -z "$W_JOB_CONCLUSION" ]; then
+  echo "Job $JOB_NAME did not complete in the given time"
+  exit 1
+elif [ "$W_JOB_CONCLUSION" != "success" ]; then
+  echo "$JOB_NAME failed with conclusion: $W_JOB_CONCLUSION"
+  exit 1
+fi
+
+echo "$JOB_NAME completed successfully"
